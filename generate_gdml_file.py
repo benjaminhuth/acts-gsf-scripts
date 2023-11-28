bfcb06b1673a320784d9f87adbc04ff4ad2a569d
Jan-Eric Heinrichs <heinrichs@physik.uni-bonn.de>
2023-11-28 17:38:27 +0100

    Added more command line arguments to customise gdml

--- generate_gdml_file.py
+++ generate_gdml_file.py
@@ -10,12 +10,15 @@ from gdml_telescope import *
 
 parser = argparse.ArgumentParser(description='Construct the GDML File for a very simple telescope geometry')
 parser.add_argument('--surfaces', help='number of telescope surfaces', type=int, default=5)
+parser.add_argument('--surface_distance', help='distance between telescope surfaces', type=int, default=50)
+parser.add_argument('--surface_thickness', help='thickness of telescope surfaces', type=int, default=1)
+parser.add_argument('--surface_width', help='width of telescope surfaces', type=int, default=1000)
 args = vars(parser.parse_args())
 
 
-surface_distance = 50
-surface_thickness = 1
-surface_width = 1000
+surface_distance = args["surface_distance"]
+surface_thickness = args["surface_thickness"]
+surface_width = args["surface_width"]
 x_offset = args["surfaces"] * surface_distance / 2
 positions = np.arange(-x_offset, args["surfaces"]*surface_distance, surface_distance).tolist()
 gdml_file = "gdml/telescope.gdml"
